71b6b5c51d8a362d3e5280c3762c76ea9c688611
Solomiia <solomiia.chunys.ik.2021@lpnu.ua>
2021-09-29 02:49:33 +0300

    Add project files.

---
+++ PR4.2/PR4.2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "  |"
+		 <<  setw(7) << "y" <<  "  |" << endl; 
+	cout << "---------------------------" << endl;
+
+	x = xp;
+	while (x <= xk)
+	{
+		A = (2 + (x * 6));
+        if (x <= 0)
+			B = ((log(cos(x)) + (x * x * x * x * x)));
+		if (0 < x && x <= 3)
+			B = (cos((1 + log(x)) / 3)) / (sin((1 + log(x)) / 3));
+		if (x > 3)
+			B = ((12 - x) - (x * x * x * x * x * x * x));
+
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+
+	return 0;
+}
